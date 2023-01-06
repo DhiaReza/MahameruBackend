0523a7db06f4a017cc4cae068300b2b02c8b5a96
Fahreza Dhia <fahrezadhia16@gmail.com>
2023-01-06 14:50:52 +0700

    combining all APIs

--- API/mahameru/__init__.py
+++ API/mahameru/__init__.py
@@ -1,15 +1,20 @@
 import os
 from flask import Flask
-from bson.json_util import dumps
-from .model import db
+from .model import db_channel
+from . import channel
 from . import user
-
+from . import chat
+from . import contact
+# file ini udah bener
 
 def create_app(test_config=None):
     app = Flask(__name__)
     
     app.config.from_pyfile('settings.cfg', silent=True)
+    app.register_blueprint(channel.bp)
     app.register_blueprint(user.bp)
+    app.register_blueprint(chat.bp)
+    app.register_blueprint(contact.bp)
 
     
     try:
@@ -17,18 +22,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    db.init_app(app)
+    db_channel.init_app(app)
 
     @app.route('/')
     def index():
         print("test")
         return "Mahameru Chat"
-    
-    @app.route('/chats')
-    def join_table():
-        joined = db.get_chat_collection()
-        hasil = dumps(joined)
-        app.logger.debug(hasil)
-        return hasil
-    
+
     return app
