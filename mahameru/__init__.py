b3eac090e0ee755bbd6510e3763529e34ad9914b
Fahreza <fahrezadhia16@gmail.com>
2023-01-08 18:56:36 +0700

    fix chat API

--- mahameru/__init__.py
+++ mahameru/__init__.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask
-from .model import db_channel
 from . import channel
 from . import user
 from . import chat
@@ -21,8 +20,6 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    db_channel.init_app(app)
 
     @app.route('/')
     def index():
